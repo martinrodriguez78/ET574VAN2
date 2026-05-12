ddcfea9f41e5be41e02b88e58cb537926232d3a2
martinrodriguez78 <martinrodriguez7898@gmail.com>
2026-05-12 19:40:14 +0000

    PE12_2

---
+++ PE12 Examples/PE12_2.py
@@ -0,0 +1,13 @@
+def Presidentstwo():
+    with open('Presidents2.txt', 'w') as presfile:
+        presfile.write('George Washington\n')
+        presfile.write('John Adams\n')
+
+def Presidentsthree():
+    with open('Presidents3.txt', 'w') as presfile:
+        presfile.write('George Washington\n')
+        presfile.write('John Adams\n')
+        presfile.write('Thomas Jefferson\n')
+
+Presidentstwo()
+Presidentsthree()
